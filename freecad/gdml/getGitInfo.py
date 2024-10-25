90b89317a28d02784a8939bece751e6bc5e5d5df
Keith Sloan <keith@sloan-home.co.uk>
2024-10-25 15:14:23 +0100

    Working but?

--- freecad/gdml/getGitInfo.py
+++ freecad/gdml/getGitInfo.py
@@ -4,23 +4,47 @@ import os
 import subprocess
 
 class gitInfo:
-    
-    def __init__(self):
+    def __init__(self, wbName, reproName):
+        self.workbenchName = wbName
         # Specify the path to your workbench directory
-        self.workbench_path = self.get_workbench_path("GDML")
+        self.workbench_path = self.get_workbench_path(reproName)
     
-    def get_workbench_path(self, workbench_name):
+    #def get_workbench_path(self):
+    def get_workbench_path(self, name):
+
         # Check if the workbench is loaded
-        if workbench_name not in FreeCADGui.listWorkbenches():
-            print(f"Workbench '{workbench_name}' is not found.")
+        if self.workbenchName not in FreeCADGui.listWorkbenches():
+            print(f"Workbench '{self.workbenchName}' is not found.")
             return None
 
+        modPath = os.path.join(FreeCAD.ConfigGet("UserAppData"), "Mod")
+        return os.path.join(modPath, name)
+        # ALL chatgpt suggestions failed now commented out
         # Get the workbench object
-        self.workbench = FreeCADGui.getWorkbench(workbench_name)
+        # workbench = FreeCADGui.getWorkbench(self.workbenchName)
+        #print(dir(workbench))
+        #
+        # Get the directory where the workbench is located
+        # self.workbench_path = os.path.dirname(workbench.__file__)
+        # return self.workbench_path
+
+        # Try to get the module associated with the workbench
+        # try:
+        #    # Get the module name from the workbench class
+        #    module_name = workbench.__class__.__module__
+
+        #   # Import the module to access its __file__ attribute
+        #   workbench_module = __import__(module_name)
+        #    self.workbench_path = os.path.dirname(workbench_module.__file__)
+        #    print(f"workbench path {self.workbench_path}")
+        #    return self.workbench_path
+        #except (AttributeError, ImportError) as e:
+        #    print(f"Error determining the path for workbench '{self.workbenchName}': {e}")
+        #    return None
 
         # Get the directory where the workbench is located
-        self.workbench_path = os.path.dirname(self.workbench.__file__)
-        return self.workbench_path
+        # self.workbench_path = os.path.dirname(workbench.__file__)
+        # return self.workbench_path
 
 
     # Function to run a git command and get the output
@@ -37,9 +61,10 @@ class gitInfo:
 
     # Get the latest commit hash
     def get_commit_hash(self):
-        return(self.run_git_command(['rev-parse', 'HEAD'], self,workbench_path))
+        return(self.run_git_command(['rev-parse', 'HEAD'], self.workbench_path))
     
 def test_functions():
-    gInfo = getInfo()
-    print(f"Branch Name {gInfo.get_branch_name()}")
-    print(f"Commit Hash: {gInfo.get_commit_hash()}")
+    gI = gitInfo("GDML_Workbench","GDML")
+    print(dir(gitInfo))
+    print(f"Branch Name: {gI.get_branch_name()}")
+    print(f"Commit Hash: {gI.get_commit_hash()}")
